aa1c6e95e99d7ebcaf4b6d87d03729157a5d0f87
medsanup400 <132312886+medsanup400@users.noreply.github.com>
2024-01-26 16:01:09 -0500

    Update SQL_Report.sql

--- SQL_Report.sql
+++ SQL_Report.sql
@@ -1,16 +1,25 @@
 
   
 select 
-a.name, 
-b.grade,
-a.marks from students a join grades b on a.marks between b.min_mark and b.max_mark
-where b.grade >= 8
+  a.name, 
+  b.grade,
+  a.marks 
+from students a 
+  join grades b on a.marks between b.min_mark and b.max_mark
+where 
+  b.grade >= 8
 
 union all 
 
 select 
-null as name, 
-b.grade,
-a.marks from students a join grades b on a.marks between b.min_mark and b.max_mark
-where b.grade < 8
-order by b.grade desc, a.name asc, a.marks asc
+  null as name, 
+  b.grade,
+  a.marks 
+from students a 
+  join grades b on a.marks between b.min_mark and b.max_mark
+where 
+  b.grade < 8
+order by 
+  b.grade desc, 
+  a.name asc, 
+  a.marks asc
